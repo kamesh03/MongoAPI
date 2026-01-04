34dc0254f7729cb82196831531de878039954c4b
Kamesh Tangudu <kamesht.03@gmail.com>
2026-01-04 16:12:18 -0500

    Changed the URI

--- FastAPI/TestMongDB.py
+++ FastAPI/TestMongDB.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-MONGO_URI = os.getenv("Mongo_URI")
+MONGO_URI = os.getenv("MONGO_URI")
 
 client=AsyncIOMotorClient(MONGO_URI)
 db=client["Mongodbtest"]
